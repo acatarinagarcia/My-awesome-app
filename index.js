6c430579c7ce8be79911a34eb0791851713d088a
acatarinagarcia <65123646+acatarinagarcia@users.noreply.github.com>
2020-06-27 16:36:36 +0100

    Implemented unit conversion

--- index.js
+++ index.js
@@ -48,25 +48,6 @@ function formatHours(timestamp) {
   return `${hours}:${minutes}`;
 }
 
-// Button Convert Temperatures: Celsius to Fahrenheit
-function choosecelsius(event) {
-  event.preventDefault();
-  let temperaturenow = document.querySelector("#current-temp");
-  temperaturenow.innerHTML = celsius;
-}
-
-function choosefahrenheit(event) {
-  event.preventDefault();
-
-  let temperaturenow = document.querySelector("#current-temp");
-  temperaturenow.innerHTML = `${Math.round((24 * 9) / 5 + 32)}`;
-}
-let fahrenheitnow = document.querySelector("#fahrenheit-link");
-fahrenheitnow.addEventListener("click", choosefahrenheit);
-
-let celsiusnow = document.querySelector("#celsius-link");
-celsiusnow.addEventListener("click", choosecelsius);
-
 //Current City Temperature
 function showWheather(response) {
   let degreesNowElement = document.querySelector("#current-temp");
@@ -78,7 +59,8 @@ function showWheather(response) {
   let dateElement = document.querySelector("#current-date");
   let hourElement = document.querySelector("#current-time");
   let iconElement = document.querySelector("#icon");
-  degreesNowElement.innerHTML = Math.round(response.data.main.temp);
+  celsiusTemperature = response.data.main.temp;
+  degreesNowElement.innerHTML = Math.round(celsiusTemperature);
   currentCityElement.innerHTML = response.data.name;
   countryElement.innerHTML = response.data.sys.country;
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -101,8 +83,8 @@ function searchCity(city) {
 
 function handleSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#search-city").value;
-  searchCity(city);
+  let city = document.querySelector("#search-city");
+  searchCity(city.value);
 }
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
@@ -121,5 +103,32 @@ function searchMyLocation() {
 
 let button = document.querySelector("#my-location");
 button.addEventListener("click", searchMyLocation);
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let degreesNowElement = document.querySelector("#current-temp");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  degreesNowElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let degreesNowElement = document.querySelector("#current-temp");
+  degreesNowElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+// Button Convert Temperatures: Celsius to Fahrenheit
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("Porto");
